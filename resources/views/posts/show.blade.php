828c1edbfb1a7c78cecaee5020e9d51abf0877c3
ritikakeni22 <ritika.keni@somaiya.edu>
2020-06-21 16:29:41 +0530

    initial release

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <a href="/posts" class="btn btn-default" >Go back</a>
+    <h2>{{$post->title}}</h2>
+    <img style ="width:100%" src="/storage/cover_image/{{$post->cover_image}}">
+    <br><br>
+    <div>
+        {!!$post->body!!}
+    </div>
+    <hr>
+    <small>Written on {{$post->created_at}}  by {{$post->user->name}}</small></li>
+    <hr>
+    @if(!Auth::guest())
+        @if(Auth::user()->id == $post->user_id)
+            <a href= "/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+            {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'Post', 'class'=>'btn float-right'])!!}
+                {{Form::hidden('_method','DELETE')}}
+                {{Form::submit('Delete', ['class' =>'btn btn-danger'])}}
+            {!!Form::close()!!}
+        @endif
+    @endif
+@endsection
